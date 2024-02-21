ebf793bbcff44a05cb478611af1ef86e5bdab8b6
Raphael Rocha <raphaelbrazao@hotmail.com>
2024-02-21 19:53:04 -0300

    Updates

---
+++ precache-manifest.c6a0dd2fc699eda83b78cb83c4e4f73a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496e80e1635009223d514a8591de4f68",
+    "url": "/VidaSelvagem/index.html"
+  },
+  {
+    "revision": "31bdfd3a2c3f345946da",
+    "url": "/VidaSelvagem/static/css/main.8dc24e3d.chunk.css"
+  },
+  {
+    "revision": "a88a22f95ee86138be42",
+    "url": "/VidaSelvagem/static/js/2.3dad13d3.chunk.js"
+  },
+  {
+    "revision": "11211063c65e8c9340e3ded9e980210e",
+    "url": "/VidaSelvagem/static/js/2.3dad13d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bdfd3a2c3f345946da",
+    "url": "/VidaSelvagem/static/js/main.ec059f2c.chunk.js"
+  },
+  {
+    "revision": "f5b903335979ac436084",
+    "url": "/VidaSelvagem/static/js/runtime-main.48df2c2a.js"
+  },
+  {
+    "revision": "a4ed75ace712191183bb96369c9fbeba",
+    "url": "/VidaSelvagem/static/media/Logo.a4ed75ac.svg"
+  },
+  {
+    "revision": "a3f7adce1c0343c298222a8dac93af00",
+    "url": "/VidaSelvagem/static/media/Socias.a3f7adce.jpg"
+  },
+  {
+    "revision": "e31e0dba13b56a9a8ae3513f3884bb92",
+    "url": "/VidaSelvagem/static/media/about-background-image.e31e0dba.png"
+  },
+  {
+    "revision": "0744e3c1b8af6599ea6cef1bb907f17a",
+    "url": "/VidaSelvagem/static/media/home-banner-background.0744e3c1.png"
+  },
+  {
+    "revision": "986c84e9d55a942259ab7259c3a41da0",
+    "url": "/VidaSelvagem/static/media/home-banner-image.986c84e9.png"
+  }
+]);
